a1da35f5e8c0a11f868340b96757c87db2727507
Tobin deKorne <tdekorne@gmail.com>
2022-03-25 10:32:20 -0700

    change images to better photos

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './portfolio.css';
-import IMG1 from '../../assets/portfolio1.jpg';
+import IMG1 from '../../assets/tic-tac-toe.png';
 import IMG6 from '../../assets/portfolio6.jpg';
 import IMG7 from '../../assets/portfolio7.jpg';
 import IMG8 from '../../assets/portfolio8.jpg';
 import IMG9 from '../../assets/portfolio9.png';
+import GOALS from '../../assets/goals.jpg';
 
 const data = [
     {
@@ -22,9 +23,9 @@ const data = [
         demo: 'https://insta2.tobindekorne.com',
     },
     {
-        id: 6,
-        image: IMG6,
-        title: 'insta-2.0',
+        id: 5,
+        image: GOALS,
+        title: 'GoalSetter',
         github: 'https://github.com/Tobindekorne/mern-goals',
         demo: 'https://mernapptobin.herokuapp.com/login',
     },
